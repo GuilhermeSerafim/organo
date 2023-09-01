717863d1ce4bec6dc18b83377e2d533e03c9067d
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-01 17:31:54 -0300

    Configurando e apagando arquivos, para comerçamos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Banner from './componentes/Banner/Banner.js';
+import Banner from "./componentes/Banner/Banner";
 
 function App() {
   //note que utilizamos um componente que criamos, através do import e marcando ele
   return (
     <div className="App">
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
