84aac654d7c3b723ef2464b6d811a32d0faaaf85
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-07 13:29:47 -0300

    Criando estrutura básica do componente Colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/guilhermeserafim.png' alt='Guilherme Serafim'/>  
+            </div>
+            <div className='rodape'>
+                <h4>Guilherme Serafim</h4>
+                <h5>CEO</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
