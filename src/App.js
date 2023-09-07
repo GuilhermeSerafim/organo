f504083ea361bfa349fa1c9265f15bff12fc2c46
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-07 01:05:59 -0300

    Criação do componente time estático

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Banner from "./componentes/Banner";
 import CampoTexto from "./componentes/CampoTexto";
 import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
   
@@ -15,6 +16,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação" />
+      <Time nome="Frontend" />
+      <Time nome="Data Science" />
     </div>
   );
 }
